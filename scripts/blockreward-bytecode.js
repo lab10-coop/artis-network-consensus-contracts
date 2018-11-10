3a57213722cccc1f2371c703a042b2e0993bd6b6
davidf <david.forstenlechner@gmx.at>
2018-11-10 21:14:48 +0100

    Added script to compile the BlockReward contract and output its bytecode

---
+++ scripts/blockreward-bytecode.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const solc = require('solc');
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("https://core.poa.network"));
+
+main();
+
+async function main() {
+	console.log('RewardByBlock compilation...');
+	const compiled = solc.compile({
+		sources: {
+			'': fs.readFileSync('../contracts/RewardByBlock.sol').toString()
+		}
+	}, 1, function (path) {
+		return {contents: fs.readFileSync('../contracts/' + path).toString()}
+	});
+	const abi = JSON.parse(compiled.contracts[':RewardByBlock'].interface);
+	let bytecode = compiled.contracts[':RewardByBlock'].bytecode;
+	
+	const contract = new web3.eth.Contract(abi);
+	const deploy = await contract.deploy({data: '0x' + bytecode, arguments: []});
+	bytecode = await deploy.encodeABI();
+	
+	console.log('PoaNetworkConsensus bytecode:');
+	console.log('');
+	console.log(bytecode);
+}
+
+// node blockreward-bytecode.js
