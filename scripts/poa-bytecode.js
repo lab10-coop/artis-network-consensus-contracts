4d4ab3c809a59d1ae863b4aa40ebebb853adbb5e
davidf <david.forstenlechner@gmx.at>
2018-11-10 08:54:42 +0100

    Set collateral to 4.5 Million in Poa Consensus Contract bytecode generating script

--- scripts/poa-bytecode.js
+++ scripts/poa-bytecode.js
@@ -18,7 +18,7 @@ async function main() {
 	let bytecode = compiled.contracts[':PoaNetworkConsensus'].bytecode;
 	
 	const contract = new web3.eth.Contract(abi);
-	const deploy = await contract.deploy({data: '0x' + bytecode, arguments: [process.env.MASTER_OF_CEREMONY, []]});
+	const deploy = await contract.deploy({data: '0x' + bytecode, arguments: [process.env.MASTER_OF_CEREMONY, [], 4500000 * 1E18]});
 	bytecode = await deploy.encodeABI();
 	
 	console.log('PoaNetworkConsensus bytecode:');
